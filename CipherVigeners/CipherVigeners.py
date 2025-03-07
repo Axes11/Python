448fb535dcfa45062b6710fdc6a507257be47e70
Axes11 <123070177+Axes11@users.noreply.github.com>
2025-03-07 21:49:42 +0100

    decipher word with key

--- CipherVigeners/CipherVigeners.py
+++ CipherVigeners/CipherVigeners.py
@@ -1,42 +1,59 @@
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 typeOfAction = input("\nВыберите что вы хотите сделать\n 1. Зашифровать \n 2. Расшифровать \n Впишите 1 либо 2: ")
-wordToCipher = input("\nВведите слово для шифрования: ")
-keyWord = input("\nВведите ключ для шифрования: ")
-
-wordToCipherList = list(wordToCipher)
-keyWordList = list(keyWord)
+wordToCipher = list(input("\nВведите слово для шифрования: "))
+keyWord = list(input("\nВведите ключ для шифрования: "))
 
 numbersOfLettersCipher = []
 numbersOfKeyWord = []
 
-resultOfSum = []
 cipheredWord = []
 
-if len(keyWordList) < len(wordToCipherList):
-    for n in range(len(wordToCipherList)):
-        keyWordList.append(keyWordList[n])
+def makeKeyWordAndWordSameLength(wordToCipherList, keyWordList):
+    newKeyWordList = []
 
-print(wordToCipherList, keyWordList)
+    if len(keyWordList) < len(wordToCipherList):
+        for j in range(len(wordToCipherList)):
+            newKeyWordList.append(keyWordList[j % len(keyWordList)])
 
-for indexWord, letterWord in enumerate(wordToCipherList) :
-    for indexKey, letterKey in enumerate(letters) :
-        if letterWord == letterKey:
-            numbersOfLettersCipher.append(indexKey)
+    return newKeyWordList
 
-for indexWord, letterWord in enumerate(keyWordList) :
-    for indexKey, letterKey in enumerate(letters) :
-        if letterWord == letterKey:
-            numbersOfKeyWord.append(indexKey)
+def replaceLettersToIndexes(wordList, letters):
+    arrayWithNewIndexes = []
 
-for i in range(len(numbersOfLettersCipher)):
-    numberInWord = numbersOfLettersCipher[i]
-    numberInKeyWord = numbersOfKeyWord[i]
+    for indexWord, letterWord in enumerate(wordList) :
+        for indexKey, letterKey in enumerate(letters) :
+            if letterWord == letterKey:
+                arrayWithNewIndexes.append(indexKey)
 
-    if numberInWord + numberInKeyWord <= 25:
-        resultOfSum.append(numberInWord + numberInKeyWord)
-    else:
-        resultOfSum.append((numberInWord + numberInKeyWord) - 26)
+    return arrayWithNewIndexes
+
+def sumTheArrays(cipheredWordList, keyWordList, action):
+    result = []
+
+    for i in range(len(cipheredWordList)):
+        numberInWord = cipheredWordList[i]
+        numberInKeyWord = keyWordList[i]
+
+        print(numberInWord, numberInKeyWord)
+
+        if action == "1":
+            if numberInWord + numberInKeyWord <= 25:
+                result.append(numberInWord + numberInKeyWord)
+            else:
+                result.append((numberInWord + numberInKeyWord) - 26)
+        elif action == "2":
+            if numberInWord - numberInKeyWord >= 0:
+                result.append(numberInWord - numberInKeyWord)
+            else:
+                result.append((numberInWord - numberInKeyWord + 26))
+
+    return result
+
+keyWordWithRightLength = makeKeyWordAndWordSameLength(wordToCipher, keyWord)
+indexesCipherWord = replaceLettersToIndexes(wordToCipher, letters)
+indexesKeyWord = replaceLettersToIndexes(keyWordWithRightLength, letters)
+resultOfSum = sumTheArrays(indexesCipherWord, indexesKeyWord, typeOfAction)
 
 for n in range(len(resultOfSum)):
     cipheredWord.append(letters[resultOfSum[n]])
